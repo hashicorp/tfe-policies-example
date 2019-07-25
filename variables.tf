06221e1a9b3a1bb2fc2f32faf2c6bf141fa44a16
ppresto <ppresto@hashicorp.com>
2019-07-24 17:16:03 -0700

    adding variables

---
+++ variables.tf
@@ -0,0 +1,19 @@
+
+variable "tfe_token" {}
+
+variable "tfe_hostname" {
+  description = "The domain where your TFE is hosted."
+  default     = "app.terraform.io"
+}
+
+variable "tfe_organization" {
+  description = "The TFE organization to apply your changes to."
+  default     = "Patrick"
+}
+
+variable "prod_workspaces" {
+  type = list
+  default = [
+    my_workspace_prod1=_1
+  ]
+}
